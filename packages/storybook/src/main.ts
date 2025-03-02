fce0d6e573406e2626f0da1cbd493a2c5b57e001
JP Grusling <jp.grusling@fusionary.com>
2025-03-02 11:12:43 -0500

    tweaks to storybook config

--- packages/storybook/src/main.ts
+++ packages/storybook/src/main.ts
@@ -1,19 +1,14 @@
-import path from 'node:path'
 import type { StorybookConfig } from '@storybook/experimental-nextjs-vite'
-
-const getAbsolutePath = (packageName: string): string =>
-  // eslint-disable-next-line unicorn/prefer-module
-  path.dirname(require.resolve(path.join(packageName, 'package.json')))
 
 export const config: StorybookConfig = {
   addons: [
-    getAbsolutePath('@storybook/addon-a11y'),
-    getAbsolutePath('@storybook/addon-links'),
-    getAbsolutePath('@storybook/addon-designs'),
-    getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-onboarding'),
-    getAbsolutePath('@storybook/addon-interactions'),
-    getAbsolutePath('@storybook/addon-viewport'),
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-designs',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+    '@storybook/addon-viewport',
   ],
   docs: {},
   framework: {
@@ -32,3 +27,6 @@ export const config: StorybookConfig = {
 }
 
 export default config
+
+// eslint-disable-next-line import/named
+export { type StorybookConfig } from '@storybook/experimental-nextjs-vite'
